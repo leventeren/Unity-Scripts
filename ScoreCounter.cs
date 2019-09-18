ea6b962d53de8837f0831a69300b38afaebef604
Levent EREN <levent@ync.com.tr>
2019-09-18 16:48:21 +0300

    Create ScoreCounter.cs

---
+++ ScoreCounter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+ using System.Collections;
+ 
+ public class ScoreCounter : MonoBehaviour 
+ {
+     public float duration = 0.5f;
+     int score = 0;
+     
+     void OnGUI () {
+         GUIButtonCountTo (0);
+         GUIButtonCountTo (5000);
+         GUIButtonCountTo (20000);
+         GUILayout.Label ("Current score is " + score);        
+     }
+ 
+     void GUIButtonCountTo (int target) {
+         if (GUILayout.Button ("Count to " + target)) {
+             StopCoroutine ("CountTo");
+             StartCoroutine ("CountTo", target);
+         }
+     }
+     
+     IEnumerator CountTo (int target) {
+         int start = score;
+         for (float timer = 0; timer < duration; timer += Time.deltaTime) {
+             float progress = timer / duration;
+             score = (int)Mathf.Lerp (start, target, progress);
+             yield return null;
+         }
+         score = target;
+     }
+ }
