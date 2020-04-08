d619dc380d653c51d7f56411e70f068f44e11a00
Levent EREN <levent@ync.com.tr>
2020-04-08 15:23:18 +0300

    Create dash.cs

---
+++ dash.cs
@@ -0,0 +1,2 @@
+rb.MovePosition(gameObject.transform.position - (gameObject.transform.forward * 5));
+//GameManager.instance.player.transform.position = GameManager.instance.player.transform.position + GameManager.instance.player.transform.forward * playerDashValue;
