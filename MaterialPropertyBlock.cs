948714aa2ddf8b6d6fb3deb49d4660190519cc31
Levent EREN <l3w3nt@gmail.com>
2022-03-18 12:12:55 +0300

    Update MaterialPropertyBlock.cs

--- MaterialPropertyBlock.cs
+++ MaterialPropertyBlock.cs
@@ -22,3 +22,43 @@ public class MaterialPropertyBlock : MonoBehaviour
 
 // Add new shader -> add to head [PerRendererData] tag to property
 // eg. [PerRendererData] _Color ("Color", Color) = (1,1,1,1)
+
+// Sample Case:
+/*
+public class MeshBall : MonoBehaviour
+{
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    [SerializeField]
+    Mesh ballmesh = default;
+    [SerializeField]
+    Material instancedMaterial = default;
+
+    Matrix4x4[] matrices = new Matrix4x4[1023];
+
+    Vector4[] colors = new Vector4[1023];
+
+    MaterialPropertyBlock materialBlock;
+
+    // Update is called once per frame
+    void Awake()
+    {
+        for (int i = 0; i < matrices.Length; i++)
+        {
+            matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 50f, Quaternion.identity, Vector3.one);
+            colors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1f));
+        }
+    }
+
+
+    void Update()
+    {
+        if (materialBlock == null)
+        {
+            materialBlock = new MaterialPropertyBlock();
+            materialBlock.SetVectorArray(baseColorId, colors);
+        }
+
+        Graphics.DrawMeshInstanced(ballmesh, 0, instancedMaterial, matrices, 1023, materialBlock);
+    }
+}
+*/
